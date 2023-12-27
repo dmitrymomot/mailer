db37f3583a41bedab68626867869b21b18242c83
Dmitry Momot <mail@dmomot.com>
2023-12-27 22:32:36 +0200

    + package DTO

---
+++ common.go
@@ -0,0 +1,33 @@
+package mailer
+
+// Predefined task names.
+const (
+	SendEmailTask = "mailer.send_email" // SendEmailTask is a task name for sending an email.
+)
+
+// Predefined task payloads.
+type (
+	// SendEmailPayload is a payload for SendEmailTask.
+	SendEmailPayload struct {
+		// Email is the email address of the recipient.
+		Email string `json:"email,omitempty"`
+		// Subject is the subject of the email.
+		Subject string `json:"subject,omitempty"`
+		// HTMLBody is the HTML body of the email.
+		HTMLBody string `json:"html_body,omitempty"`
+		// Attachments is an optional encoded file to send along with an email (see Attachment).
+		Attachments []Attachment `json:"attachments,omitempty"`
+	}
+
+	// Attachment is an optional encoded file to send along with an email
+	Attachment struct {
+		// Name: attachment name
+		Name string `json:"name,omitempty"`
+		// Content: Base64 encoded attachment data
+		Content string `json:"content,omitempty"`
+		// ContentType: attachment MIME type
+		ContentType string `json:"content_type,omitempty"`
+		// ContentId: populate for inlining images with the images cid
+		ContentID string `json:"content_id,omitempty"`
+	}
+)
