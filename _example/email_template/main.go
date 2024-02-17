c4a2aac92d05607be5235843e2a417ecf10fa4dc
Dmitry Momot <mail@dmomot.com>
2024-02-18 01:56:22 +0200

    Update email template with new content

--- _example/email_template/main.go
+++ _example/email_template/main.go
@@ -23,20 +23,35 @@ func emailPreview(w http.ResponseWriter, r *http.Request) {
 	err := mailer_template.Main(
 		mailer_template.MainData{
 			Title:     "Test Email Preview",
-			Preheader: "This is a test email preview",
+			Preheader: "Enim deserunt non deserunt deserunt commodo cupidatat excepteur esse ad laborum.",
 		},
 		mailer_template.Container(
-			mailer_template.Alert("This is a test email preview"),
+			mailer_template.Alert("Aliqua adipisicing enim irure do consectetur nostrud exercitation consectetur commodo."),
 		),
 		mailer_template.Container(
-			mailer_template.AlertOutlined("This is a test email preview"),
+			mailer_template.AlertOutlined("Dolore exercitation voluptate cupidatat cillum quis exercitation irure adipisicing ex."),
+		),
+		mailer_template.Card(
+			mailer_template.SubHeader("Hi there!"),
+			mailer_template.Paragraph("Aute do aliqua consequat nulla ad adipisicing aliquip adipisicing exercitation esse. Ipsum amet consequat aliqua culpa excepteur sint anim dolor duis magna et dolor. Dolore Lorem ut dolor aliquip amet sint. Nostrud deserunt sunt elit amet qui consequat sint sit. Nostrud laborum dolore occaecat officia ex culpa officia cillum occaecat proident amet laboris culpa anim."),
+			mailer_template.ButtonPrimary(mailer_template.ButtonAlignCenter, "Confirm email", "https://example.com"),
+			mailer_template.Paragraph("Nulla excepteur ex eiusmod ad cupidatat laborum commodo consectetur nulla. Eu anim consectetur esse cupidatat irure irure. Anim aliqua irure eiusmod dolor Lorem non. Ea reprehenderit ex sunt in nulla fugiat amet ea <a href=\"http://example.com\">aliquip aliqua ad</a>."),
+			mailer_template.List(
+				"Lorem ipsum dolor sit amet",
+				"Consectetur adipiscing elit",
+				"Sed do eiusmod tempor incididunt",
+				"Ut labore et dolore magna aliqua",
+			),
+			mailer_template.Paragraph("Reprehenderit ea excepteur cillum aute aliqua. Incididunt ex do anim ex fugiat eiusmod incididunt est culpa officia et dolore. Qui ipsum voluptate sunt nisi irure eiusmod incididunt dolore elit enim anim."),
+			mailer_template.Divider(),
+			mailer_template.ParagraphSecondary("Aute do aliqua consequat nulla ad adipisicing aliquip adipisicing exercitation esse. Ipsum amet consequat aliqua culpa excepteur sint anim dolor duis magna et dolor. Dolore Lorem ut dolor aliquip amet sint."),
 		),
 		mailer_template.Card(
 			mailer_template.Header("Test Email Preview"),
 			mailer_template.Paragraph("Aute do aliqua consequat nulla ad adipisicing aliquip adipisicing exercitation esse. Ipsum amet consequat aliqua culpa excepteur sint anim dolor duis magna et dolor. Dolore Lorem ut dolor aliquip amet sint. Nostrud deserunt sunt elit amet qui consequat sint sit. Nostrud laborum dolore occaecat officia ex culpa officia cillum occaecat proident amet laboris culpa anim."),
-			mailer_template.ButtonPrimary(mailer_template.ButtonAlignCenter, "Click me", "https://example.com"),
+			mailer_template.ButtonDanger(mailer_template.ButtonAlignCenter, "Click me", "https://example.com"),
 			mailer_template.Divider(),
-			mailer_template.ButtonOutlinedPrimary(mailer_template.ButtonAlignCenter, "Click me", "https://example.com"),
+			mailer_template.ButtonOutlinedDanger(mailer_template.ButtonAlignCenter, "Click me", "https://example.com"),
 			mailer_template.CouponDashed(mailer_template.Coupon{
 				Persentage:     10,
 				Code:           "TESTCODE",
